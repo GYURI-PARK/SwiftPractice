cbf83e9b2658ad6215a05f2cd1d7065f3ac97aa7
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2022-10-21 00:02:26 +0900

    2022.10.20

--- Practice/main.swift
+++ Practice/main.swift
@@ -16,9 +16,43 @@ class Person {
     var age: Int = 20
 }
 
+
 let orange: Person = Person()
 
 print("(1) use print : ", orange)
 print("\n")
 print("(2) use dump : ")
 dump(orange)
+
+
+print("==================================")
+
+// 상수와 변수
+
+let constant: String = "차후에 변경이 불가능한 상수 let"
+var variable: String = "차후에 변경이 가능한 변수 var"
+print(constant)
+print(variable)
+
+print("\n")
+
+variable = "변수는 이렇게 차후에 다른 값을 할당 할 수 있지만"
+print(variable)
+
+// 상수 선언 후 나중에 값 할당하기
+print("\n")
+print("\n")
+
+let sum: Int
+let inputA: Int = 100
+let inputB: Int = 200
+
+sum = inputA + inputB
+print(sum)
+
+// 변수 선언 후 나중에 값 할당하기
+var nickName: String
+nickName = "귤"
+
+print(nickName)
+
