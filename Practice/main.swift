d2daaaf2777172e0001747df844f8eacc2933bf6
GYURI-PARK <93391058+GYURI-PARK@users.noreply.github.com>
2022-10-20 23:20:05 +0900

    2022.10.20

--- Practice/main.swift
+++ Practice/main.swift
@@ -1,11 +1,24 @@
-//
-//  main.swift
-//  Practice
-//
-//  Created by GYURI on 2022/10/20.
-//
+// 콘솔로그와 문자열 보간법
+import Swift
+let age: Int = 20
+print("안녕하세요! 저는 \(age)살 입니다:)")
 
-import Foundation
+print("안녕하세요! 저는 \(age+4)살 입니다:)")
 
-print("Hello, World!")
 
+print("==================================")
+print("\n")
+print("\n")
+// dump 사용
+
+class Person {
+    var name: String = "gyu ri"
+    var age: Int = 20
+}
+
+let orange: Person = Person()
+
+print("(1) use print : ", orange)
+print("\n")
+print("(2) use dump : ")
+dump(orange)
